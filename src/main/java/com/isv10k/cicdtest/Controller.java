d80a0a22a0f7c16ce234f40c9abbcd045511ff45
Sergey Ilchenko <isv10k@gmail.com>
2022-11-30 09:13:43 +0500

    simple controller

---
+++ src/main/java/com/isv10k/cicdtest/Controller.java
@@ -0,0 +1,18 @@
+package com.isv10k.cicdtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/api")
+    public String helloWorld(@RequestParam(required = false) String name) {
+        if (name == null) {
+            return "Hello world!";
+        } else {
+            return "Hello " + name + "!";
+        }
+    }
+}

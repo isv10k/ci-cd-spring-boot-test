a88159d1640a3d6f30a38b8cb2bcc1abde8e3a3e
Sergey Ilchenko <isv10k@gmail.com>
2022-11-30 08:56:55 +0500

    initial proj

---
+++ src/test/java/com/isv10k/cicdtest/CiCdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isv10k.cicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
